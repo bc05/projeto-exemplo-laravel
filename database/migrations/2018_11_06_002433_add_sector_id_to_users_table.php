efb4d165f8b98a569618cef0b02f87746d66bc0f
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2018-11-06 07:54:08 -0200

    fix error migration MySQL

--- database/migrations/2018_11_06_002433_add_sector_id_to_users_table.php
+++ database/migrations/2018_11_06_002433_add_sector_id_to_users_table.php
@@ -15,10 +15,23 @@ class AddSectorIdToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             if (!Schema::hasColumn('users', 'sector_id')) {
-                $table->smallInteger('sector_id');
+                $table->smallInteger('sector_id')->unsigned();
                 $table->foreign('sector_id')->references('id')->on('sector');
             }
         });
+        //ou tente isto 
+        //desconmentar todo o código abaixo e comentar o bloco Schema acima
+        // Schema::table('users', function (Blueprint $table) {
+        //     if (!Schema::hasColumn('users', 'sector_id')) {
+        //         $table->smallInteger('sector_id')->unsigned();
+        //     }
+        // });
+
+        // Schema::table('users', function (Blueprint $table) {
+        //     if (!Schema::hasColumn('users', 'sector_id')) {
+        //         $table->foreign('sector_id')->references('id')->on('sector');
+        //     }
+        // });
     }
 
     /**
